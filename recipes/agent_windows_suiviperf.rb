90a893e1ab36c5e8119bf87c14a879c71c2a4b3b
Guilhem Lettron <guilhem@lettron.fr>
2013-01-07 16:44:49 +0100

    suiviperf is now version 2.0.4

--- recipes/agent_windows_suiviperf.rb
+++ recipes/agent_windows_suiviperf.rb
@@ -2,9 +2,9 @@ include_recipe "windows"
 
 windows_package "zabbix" do
   if node['kernel']['machine'] == "x86"
-    source "http://www.suiviperf.com/zabbix/zabbix_agent-2.0.2_x86.msi"
+    source "http://www.suiviperf.com/zabbix/zabbix_agent-2.0.4_x86.msi"
   elsif  node['kernel']['machine'] == "x86_64"
-    source "http://www.suiviperf.com/zabbix/zabbix_agent-2.0.2_x64.msi"
+    source "http://www.suiviperf.com/zabbix/zabbix_agent-2.0.4_x64.msi"
   end
   installer_type :msi
   options "/qn"
