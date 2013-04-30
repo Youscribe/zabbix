a7c0a8eb6505c9ca226a60bba61f3609d8db40c9
Guilhem Lettron <guilhem@lettron.fr>
2013-04-30 12:41:26 +0200

    add recipe agent_windows_suiviperf

---
+++ recipes/agent_windows_suiviperf.rb
@@ -0,0 +1,24 @@
+include_recipe "windows"
+
+windows_package "zabbix" do
+  if node['kernel']['machine'] == "x86"
+    source "http://www.suiviperf.com/zabbix/zabbix_agent-2.0.4_x86.msi"
+  elsif  node['kernel']['machine'] == "x86_64"
+    source "http://www.suiviperf.com/zabbix/zabbix_agent-2.0.4_x64.msi"
+  end
+  installer_type :msi
+  options "/qn"
+  action :install
+end
+
+service "zabbix_agentd" do
+  service_name "Zabbix Agent"
+  provider Chef::Provider::Service::Windows
+  supports :restart => true
+  action [ :enable, :start ]
+end
+
+template "C:/Program Files/Zabbix Agent/zabbix_agentd.conf" do
+  source "zabbix_agentd.conf.erb"
+  notifies :restart, "service[zabbix_agentd]"
+end
